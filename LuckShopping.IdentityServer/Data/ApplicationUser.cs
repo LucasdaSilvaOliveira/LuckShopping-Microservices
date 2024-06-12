c8bc47838391c35bac1cf9b2e8b52360ed87a45e
Lucas da Silva Oliveira <lucas-silva-98@hotmail.com>
2024-06-12 18:08:49 -0300

    feat: Implementação novo projeto para o Identity Server com o Duende Framework.

---
+++ LuckShopping.IdentityServer/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LuckShopping.IdentityServer.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private string FirstName { get; set; }
+
+        private string LastName { get; set; }
+    }
+}
---
+++ LuckShopping.IdentityServer/Data/Context/LocalContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LuckShopping.IdentityServer.Data.Context
+{
+    public class LocalContext : IdentityDbContext<ApplicationUser>
+    {
+        public LocalContext(DbContextOptions<LocalContext> opt) : base(opt)
+        {
+            
+        }
+    }
+}
